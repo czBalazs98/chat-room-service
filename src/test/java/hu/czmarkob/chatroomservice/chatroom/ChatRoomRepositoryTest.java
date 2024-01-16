6f391149c1058c7fb0ed8178f20abc89df9e03c7
bczmarko <bczmarko@intrend.hu>
2024-01-16 15:42:07 +0100

    added tests for ChatRoomRepository and ChatRoomService

---
+++ src/test/java/hu/czmarkob/chatroomservice/chatroom/ChatRoomRepositoryTest.java
@@ -0,0 +1,49 @@
+package hu.czmarkob.chatroomservice.chatroom;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+
+@DataJpaTest
+public class ChatRoomRepositoryTest {
+
+	@Autowired
+	private ChatRoomRepository chatRoomRepository;
+
+	@BeforeEach
+	void set_up() {
+		ChatRoom chatRoom = generateChatRoom();
+		chatRoomRepository.save(chatRoom);
+	}
+
+	@Test
+	void find_by_name_contains() {
+		List<ChatRoom> chatRooms = chatRoomRepository.findByNameContains("test");
+
+		assertThat(chatRooms).isNotEmpty();
+		assertThat(chatRooms.get(0).getId()).isEqualTo(1L);
+	}
+
+	@Test
+	void find_by_name_contains_no_results() {
+		List<ChatRoom> chatRooms = chatRoomRepository.findByNameContains("not_exists");
+
+		assertThat(chatRooms).isEmpty();
+	}
+
+	public static ChatRoom generateChatRoom() {
+		ChatRoom chatRoom = new ChatRoom();
+		chatRoom.setId(1L);
+		chatRoom.setName("test chatroom");
+		chatRoom.setRules("1.) test rule");
+		chatRoom.setTags(List.of("test1", "test2"));
+		chatRoom.setDescription("test description");
+		return chatRoom;
+	}
+}
---
+++ src/test/java/hu/czmarkob/chatroomservice/chatroom/ChatRoomServiceImplTest.java
@@ -0,0 +1,70 @@
+package hu.czmarkob.chatroomservice.chatroom;
+
+import static org.mockito.BDDMockito.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ChatRoomServiceImplTest {
+
+	private static final String CHAT_ROOM_NAME = "test";
+
+	@Mock
+	private ChatRoomRepository chatRoomRepository;
+
+	private ChatRoomServiceImpl chatRoomService;
+
+	private final ChatRoomDtoMapper mapper = Mappers.getMapper(ChatRoomDtoMapper.class);
+
+	private ChatRoom chatRoom;
+
+	private ChatRoomDto chatRoomDto;
+
+	@BeforeEach
+	void setUp() {
+		chatRoomService = new ChatRoomServiceImpl(chatRoomRepository);
+
+		chatRoom = ChatRoomRepositoryTest.generateChatRoom();
+		chatRoomDto = mapper.toDto(chatRoom);
+	}
+
+	@Test
+	void test_find_chat_rooms() {
+		given(chatRoomRepository.findAll()).willReturn(List.of(chatRoom));
+
+		List<ChatRoomDto> chatRooms = chatRoomService.findChatRooms();
+
+		assertThat(chatRooms).hasSize(1);
+		verify(chatRoomRepository, times(1)).findAll();
+	}
+
+	@Test
+	void test_find_chat_rooms_by_name_contains() {
+		given(chatRoomRepository.findByNameContains(CHAT_ROOM_NAME)).willReturn(List.of(chatRoom));
+
+		List<ChatRoomDto> chatRooms = chatRoomService.findChatRoomsByName(CHAT_ROOM_NAME);
+
+		assertThat(chatRooms).hasSize(1);
+		verify(chatRoomRepository, times(1)).findByNameContains(CHAT_ROOM_NAME);
+	}
+
+	@Test
+	void test_create_chat_room() {
+		given(chatRoomRepository.save(chatRoom)).willReturn(chatRoom);
+
+		ChatRoomDto createdChatRoomDto = chatRoomService.createChatRoom(chatRoomDto);
+
+		assertThat(createdChatRoomDto).isEqualTo(chatRoomDto);
+		verify(chatRoomRepository, times(1)).save(chatRoom);
+	}
+}
